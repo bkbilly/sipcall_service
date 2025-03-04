73268936162863a3d62a49a0fac30bdf2f4606b1
bkbilly <bkbilly@hotmail.com>
2025-03-04 22:56:37 +0200

    Add sounds from file

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from threading import Thread
 from typing import Union
 import subprocess
+import glob
 import time
 import sys
+import os
 
 import yaml
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 
 with open("/opt/sipcall/config.yaml", "r", encoding="utf8") as file:
@@ -24,7 +26,15 @@ def read_root():
 
 
 @app.post("/makecall/{phone}")
-def read_item(phone: str, audiofile: str = "/opt/sipcall/audio/play2.wav"):
+def read_item(phone: str, request: Request):
+    # print(dict(request.query_params))
+    desc = request.query_params.get("desc").replace("+", " ")
+    audiofile = "/opt/sipcall/audio/play2.wav"
+    for file in glob.glob("/opt/sipcall/audio/*.wav"):
+        filename = os.path.splitext(os.path.basename(file))[0]
+        if filename in desc:
+            audiofile = file
+
     command = ["/bin/sipcall",
         "-sd", conf["domain"],
         "-su", conf["username"],
